a731e222b60f7995ca896475e91164edaf5fdfc1
yoshino9397 <88402243+yoshino9397@users.noreply.github.com>
2021-08-17 15:25:18 -0700

    Java Script
    簡単なJS

---
+++ Day3 Lab/js/main.js
@@ -0,0 +1,41 @@
+{
+  var x = document.getElementById("animal");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML="<b>Dog</b>";
+
+  var x = document.getElementById("animal");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML="<b>Cat</b>";
+
+  var x = document.getElementById("animal");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML="<b>Horse</b>";
+
+  var x = document.getElementById("animal");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML="<b>Snake</b>";
+  
+  var x = document.getElementById("animal");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML="<b>Rubbit</b>";
+  
+  var x = document.getElementById("sum");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML=49 + 20 -19;
+
+  var x = document.getElementById("sum");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML=30 + 20 * 10;
+
+  var x = document.getElementById("sum");
+  var y = document.createElement("p");
+  x.appendChild(y);
+  y.innerHTML=40 + 20 -10;
+}

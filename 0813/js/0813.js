654ce027de2a730b75be2a09c28b517316b361ca
yoshino9397 <88402243+yoshino9397@users.noreply.github.com>
2021-08-17 15:26:26 -0700

    Javascript
    簡単なJS

---
+++ 0813/js/0813.js
@@ -0,0 +1,43 @@
+{
+function add (number1,number2) {
+    return number1 + number2;
+   }
+   document.getElementById("demo").innerHTML = add(3,2);
+   document.getElementById("demo1").innerHTML = add(-6,-3);
+   document.getElementById("demo2").innerHTML = add(7,3);
+   
+function incre (number) {
+    return number + 1;
+   }
+   document.getElementById("inc").innerHTML = incre(0);
+   document.getElementById("inc1").innerHTML = incre(9);
+   document.getElementById("inc2").innerHTML = incre(-3);
+
+function minutes (number) {
+    return number * 60;
+   }
+   document.getElementById("min").innerHTML = minutes(1);
+   document.getElementById("min1").innerHTML = minutes(10);
+   document.getElementById("min2").innerHTML = minutes(30);
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
